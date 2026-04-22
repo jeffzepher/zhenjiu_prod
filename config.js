a92f17289392e31cf50672565e558c6c1a83bfe0
jeffzepher <jeffchen9618@gmail.com>
2026-04-22 10:26:46 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js（不要提交到版本控制）
+window.APP_CONFIG = {
+	SUPABASE_URL : 'https://woimziponrsxkgrdqasa.supabase.co',
+    SUPABASE_ANON_KEY : 'sb_publishable_aHK3etRyH_8k1vNQGdRUcg_v7-vTUwf'
+};
