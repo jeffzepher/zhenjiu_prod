c577f9fe145a815b162583de91a1875c6ce842f7
jeffzepher <jeffchen9618@gmail.com>
2026-04-22 10:25:56 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js（不要提交到版本控制）
-window.APP_CONFIG = {
-	SUPABASE_URL : 'https://woimziponrsxkgrdqasa.supabase.co',
-    SUPABASE_ANON_KEY : 'sb_publishable_aHK3etRyH_8k1vNQGdRUcg_v7-vTUwf'
-};
